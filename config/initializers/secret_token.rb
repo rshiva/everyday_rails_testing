06d4fcfacfb50a457f0e8933f7e618549c5246de
rshiva <nadarshiva.r@gmail.com>
2013-08-17 15:16:25 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EverydayRails::Application.config.secret_token = '84db7f4b20b32049d2dc7d0bec8c1506b8901841ce65227bcf00efc68d7faa85ae526b71f0e2684085f304a7c81c8fafe585e17430dd635f950a9b4f05a1b153'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+EverydayRails::Application.config.session_store :cookie_store, key: '_everyday_rails_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# EverydayRails::Application.config.session_store :active_record_store
