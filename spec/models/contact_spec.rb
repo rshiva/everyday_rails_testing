b075aac9444866c22f31db09a6d55036543c32c9
shiva <nadarshiva.r@gmail.com>
2013-09-04 19:01:51 +0530

    moved to factory girl chap 4

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,6 +1,11 @@
 require "spec_helper"
 
 describe Contact do
+  
+  it "has a valid factory" do
+      expect(build(:contact)).to be_valid
+  end
+
   it "is valid with a firstname,lastname and email" do
   	contact=Contact.new(
   		firstname: 'shiva',
@@ -10,32 +15,33 @@ describe Contact do
   end
 
   it "is invalid without a firstname" do
-  	expect(Contact.new(firstname: nil)).to have(1).errors_on(:firstname)
+  #	expect(Contact.new(firstname: nil)).to have(1).errors_on(:firstname)
+  contact=FactoryGirl.build(:contact,lastname: nil)
+  expect(contact).to have(1).errors_on(:lastname)
   end
 
   it "is invalid without a lastname" do
-  	expect(Contact.new(lastname: nil)).to have(1).errors_on(:lastname)
+  expect(Contact.new(lastname: nil)).to have(1).errors_on(:lastname)
+  contact=FactoryGirl.build(:contact,lastname: nil)
+  expect(contact).to have(1).errors_on(:lastname)
+
   end
 
   it "is invalid without a email address" do
   	expect(Contact.new(email: nil)).to have(1).errors_on(:email)
-  	
+  contact=FactoryGirl.build(:contact,email: nil)
+  expect(contact).to have(1).errors_on(:email)
   end
 
   it "is invalid with a duplicate email address" do
-  	Contact.create(
-  		firstname: 'shiva',
-  		lastname: 'kumar',
-  		email: "tester@gmail.com")
-  	contact=Contact.new(
-  		firstname: 'jim',
-  		lastname: 'ray',
-  		email: "tester@gmail.com")
+    create(:contact,:email => "shiva@example.com")
+    contact=build(:contact, email: "shiva@example.com")
     expect(contact).to have(1).errors_on(:email)
   end
 
   it "returns a contact's full name as a string" do
-  	contact=Contact.new(firstname: "shiva" , lastname: "kumar", email: "tester@gmail.com" )
+#  	contact=Contact.new(firstname: "shiva" , lastname: "kumar", email: "tester@gmail.com" )
+    contact=FactoryGirl.build(:contact, firstname: "shiva",lastname: "kumar")
   	expect(contact.name).to eql("shiva kumar")
   end
  
